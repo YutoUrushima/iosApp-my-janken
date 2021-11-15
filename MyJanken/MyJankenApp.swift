02e3951ee35858e17057289d838526b38fc6e61a
UrushimaYuto <urushima@details.co.jp>
2021-11-15 14:20:30 +0900

    Initial Commit

---
+++ MyJanken/MyJankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJankenApp.swift
+//  MyJanken
+//
+//  Created by sd037 on 2021/11/15.
+//
+
+import SwiftUI
+
+@main
+struct MyJankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
